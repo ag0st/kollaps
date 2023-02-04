20817d37e980d075ffa02423460cd72bc772f600
romain.agost <romain.agostinelli@gmail.com>
2023-02-04 18:39:10 +0100

    update the update binaries

--- binaries/update-binaries.sh
+++ binaries/update-binaries.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cd ..
 cargo build --release
 
 if [[ -f binaries/reporter ]]; then
@@ -15,3 +16,5 @@ fi
 cp target/release/reporter binaries/
 cp target/release/runner binaries/
 cp target/release/client binaries/
+
+cd binaries
