6b9210e50491df8d88b6c92579ee584c6b40f4d0
romain.agost <romain.agostinelli@gmail.com>
2023-02-04 17:11:49 +0100

    update for ubuntu

---
+++ binaries/launch.sh
@@ -0,0 +1,1 @@
+sudo ./runner --ip-address 10.0.2.15 --local-speed 9999999 --subnet 10.0.2.0/24 --gateway 10.0.2.2 --interface enp1s0 --leader true --reporter-exec-path /home/kollap/kollaps/binaries/reporter
